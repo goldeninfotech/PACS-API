449b183e4d9f108e1861312af60655ccbeb160cc
Hridoy Roy <roy.hridoy2014work@outlook.com>
2023-12-06 18:54:20 +0600

    Update Department CRUD

--- GDNTRDSolution_API/Areas/TRD/Controllers/DepartmentController.cs
+++ GDNTRDSolution_API/Areas/TRD/Controllers/DepartmentController.cs
@@ -17,12 +17,85 @@ namespace GDNTRDSolution_API.Areas.TRD.Controllers
             _department = department;
         }
 
+        #region Department CRUD
         [HttpGet]
         [Route("GetDepartmentList")]
         public IActionResult GetDepartmentList(int pageNumber = 1, int limit = 10)
         {
             var data = _department.GetDepartmentList();
-            return Ok(data);
+            IEnumerable<Departments> paginatedData;
+            if (limit == 0)
+                paginatedData = data.Skip(pageNumber - 1);
+            else
+                paginatedData = data.Skip((pageNumber - 1) * limit).Take(limit);
+
+            var response = ReturnData.ReturnDataList(data);
+            var result = new ListMetaData<Departments>
+            {
+                TotalData = data.Count(),
+                DataFound = paginatedData.Count(),
+                CurrentPage = pageNumber,
+                TotalPages = (int)Math.Ceiling((double)data.Count() / limit),
+                DataLimit = limit,
+                Data = paginatedData
+            };
+            return Ok(new { IsSuccess = response.IsSuccess, Message = response.Message, resultData = result });
         }
+
+        [HttpGet]
+        [Route("GetDepartmentById")]
+        public IActionResult GetDepartmentById(int id)
+        {
+            var data = _department.GetDepartmentById(id);
+            var response = ReturnData.ReturnDataListById(data);
+            return Ok(new { IsSuccess = response.IsSuccess, Message = response.Message, resultData = data });
+        }
+
+
+        [HttpPost]
+        [Route("SaveDepartmentInfo")]
+        public async Task<IActionResult> SaveDepartmentInfo(Departments model ) 
+        {
+            if (ModelState.IsValid)
+            {
+                model.Status = "1";
+                model.AddedBy = "";
+                model.AddedDate = DateTime.Now.ToString("dd-mm-yyyy");
+                var data = await _department.SaveDepartmentInfo(model);
+                return Ok(data);
+            }
+            else
+                return BadRequest();
+        }
+
+        [HttpPut]
+        [Route("UpdateDepartmentInfo")]
+        public async Task<IActionResult> UpdateDepartmentInfo(Departments model, int subMenuId, string type)
+        {
+            if (model.Id > 0)
+            {
+                model.UpdatedDate = DateTime.Now.ToString("dd-mm-yyyy");
+                model.UpdatedBy ="";
+                var data = await _department.UpdateDepartmentInfo(model);
+                return Ok(data);
+            }
+            else
+                return BadRequest();
+        }
+
+        [HttpDelete]
+        [Route("DeleteDepartmentInfo")]
+        public async Task<IActionResult> DeleteDepartmentInfo(int id)
+        {
+            if (id > 0)
+            {
+                var data = await _department.DeleteDepartmentInfo(id);
+                return Ok(data);
+            }
+            else
+                return BadRequest();
+        }
+
+        #endregion
     }
 }
--- SoftEngine.TRDCore/TRD/DepartmentBLL.cs
+++ SoftEngine.TRDCore/TRD/DepartmentBLL.cs
@@ -30,21 +30,144 @@ namespace SoftEngine.TRDCore.TRD
         }
         public Departments GetDepartmentById(int id)
         {
-            throw new NotImplementedException();
+            using (var connection = new SqlConnection(_dbSettings.DefaultConnection))
+            {
+                StringBuilder strSql = new StringBuilder();
+                strSql.AppendLine(" Select Id,Name,Status from Departments where Status=1 ");
+                strSql.AppendLine(" and Id=@Id ");
+                var models = connection.Query<Departments>(strSql.ToString(), new { Id = id, }).FirstOrDefault(); 
+                return models;
+            }
         }
 
-        public Task<DataBaseResponse> SaveDepartmentInfo(Departments model)
+        public async Task<DataBaseResponse> SaveDepartmentInfo(Departments model)
         {
-            throw new NotImplementedException();
+            var response = new DataBaseResponse();
+            await using (var connection = new SqlConnection(_dbSettings.DefaultConnection))
+            {
+                bool duplicateresult = GetDuplicateCOA1(model.Name, 0);
+                if (duplicateresult)
+                {
+                    StringBuilder strSql = new StringBuilder();
+                    strSql.AppendLine(" INSERT INTO  Departments");
+                    strSql.AppendLine(" ( Name,Status,AddedBy,AddedDate) VALUES ");
+                    strSql.AppendLine(" ( @Name,@Status,@AddedBy,@AddedDate);");
+                    try
+                    {
+                        var Saveresult = connection.Execute(strSql.ToString(),
+                                        new
+                                        {
+                                            Name = model.Name,
+                                            Status = model.Status,
+                                            AddedBy = model.AddedBy,
+                                            AddedDate = model.AddedDate,
+                                        }
+                                        );
+                        response.ReturnValue = Saveresult;
+                        response.Message = GlobalConst.INSERT_SUCCESS_MESSAGE;
+                        response.IsSuccess = true;
+                    }
+                    catch (Exception exp)
+                    {
+                        response.ReturnValue = -1;
+                        response.Message = exp.Message;
+                        response.IsSuccess = false;
+                    }
+                }
+                else
+                {
+                    response.ReturnValue = -1;
+                    response.Message = GlobalConst.GET_DUPLICATEDATA;
+                    response.IsSuccess = false;
+                }
+            }
+            return response;
         }
 
-        public Task<DataBaseResponse> UpdateDepartmentInfo(Departments model)
+        public async Task<DataBaseResponse> UpdateDepartmentInfo(Departments model)
         {
-            throw new NotImplementedException();
+            var response = new DataBaseResponse();
+            await using (var connection = new SqlConnection(_dbSettings.DefaultConnection))
+            {
+                bool duplicateresult = GetDuplicateCOA1(model.Name, model.Id);
+                if (duplicateresult)
+                {
+                    StringBuilder strSql = new StringBuilder();
+                    strSql.AppendLine(" UPDATE  Departments SET ");
+                    strSql.AppendLine(" Name=@Name,Status=@Status, UpdatedBy=@UpdatedBy,UpdatedDate=@UpdatedDate ");
+                    strSql.AppendLine(" Where Id=@Id;");
+                    try
+                    {
+                        var Saveresult = connection.Execute(strSql.ToString(),
+                                        new
+                                        {
+                                            Id = model.Id,
+                                            Name = model.Name,
+                                            Status = model.Status,
+                                            UpdatedBy = model.UpdatedBy,
+                                            UpdatedDate = model.UpdatedDate
+                                        }
+                                        );
+                        response.ReturnValue = Saveresult;
+                        response.Message = GlobalConst.UPDATE_SUCCESS_MESSAGE;
+                        response.IsSuccess = true;
+                    }
+                    catch (Exception exp)
+                    {
+                        response.ReturnValue = -1;
+                        response.Message = exp.Message;
+                        response.IsSuccess = false;
+                    }
+                }
+                else
+                {
+                    response.ReturnValue = -1;
+                    response.Message = GlobalConst.GET_DUPLICATEDATA;
+                    response.IsSuccess = false;
+                }
+            }
+            return response;
         }
-        public Task<DataBaseResponse> DeleteDepartmentInfo(int id)
+        public async Task<DataBaseResponse> DeleteDepartmentInfo(int id)
         {
-            throw new NotImplementedException();
+            var response = new DataBaseResponse();
+            await using (var connection = new SqlConnection(_dbSettings.DefaultConnection))
+            {
+                StringBuilder strSql = new StringBuilder();
+                strSql.AppendLine(" UPDATE Departments SET Status=@Status WHERE Id=@Id");
+                try
+                {
+                    var result = connection.Execute(strSql.ToString(), new { Status = "0", Id = id, });
+                    response.ReturnValue = result;
+                    response.Message = GlobalConst.DELETE_SUCCESS_MESSAGE;
+                    response.IsSuccess = true;
+                }
+                catch (Exception exp)
+                {
+                    response.ReturnValue = -1;
+                    response.Message = exp.Message;
+                    response.IsSuccess = false;
+                }
+            }
+            return response;
+        }
+
+        public bool GetDuplicateCOA1(string name, int id)
+        {
+            using (var connection = new SqlConnection(_dbSettings.DefaultConnection))
+            {
+                var sql = @" Select Id,Name,Status from Departments where Status=1  ";
+                if (!string.IsNullOrEmpty(name))
+                    sql += @" and REPLACE(LOWER(Name), ' ', '') =REPLACE('" + name + @"', ' ', '') ";
+                if (id > 0)
+                    sql += @" and Id!=" + id + " ";
+
+                var models = connection.Query<Departments>(sql);
+                if (models.Count() == 0)
+                    return true;
+                else
+                    return false;
+            }
         }
 
     }
